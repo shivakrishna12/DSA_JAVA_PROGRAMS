8680dd61a55ebd7453e1f43547ef21818e7d46ad
shivakrishna12 <shivakrishna.d16@gmail.com>
2022-09-02 06:51:18 +0530

    Reverse Array Program by using two pointers

---
+++ Arrays/ReverseArray.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+public class ReverseArray{
+	
+	public static void main(String[] args){
+		int[] array=new int[]{10,20,15,3,12,40};
+		
+		System.out.println(Arrays.toString(reverse(array)));
+			}
+	public static int[] reverse(int[] array){
+		int start=0;
+		int end=array.length-1;
+		
+		while(start <=end){		
+			int temp=array[start];
+			array[start]=array[end];
+			array[end]=temp;
+			start++;
+			end--;
+		}
+		return array;
+	}
+	
+}
