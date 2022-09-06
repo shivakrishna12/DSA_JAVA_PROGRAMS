d257bf0ac29d1e510571a213025da83b45a702ce
shivakrishna12 <shivakrishna.d16@gmail.com>
2022-09-06 09:05:46 +0530

    new files

---
+++ Arrays/KadanesAlgo2.java
@@ -0,0 +1,22 @@
+public class KadanesAlgo2 {
+    public static void main(String[] args){
+        int[] nums={-3,2,-1,4,-2};
+       System.out.println("MaxSum::" +findMaxSumSubArray(nums));
+
+    }
+public static int findMaxSumSubArray(int[] nums){
+      int maxSum=Integer.MIN_VALUE;
+    for(int i=0;i<nums.length;i++){
+        int subSum=0;
+        for(int j=i;j<nums.length;j++){
+             subSum=subSum+nums[j];
+             if(subSum > maxSum){
+                maxSum=subSum;
+         }
+        }
+        
+      
+    }
+    return maxSum;
+}
+}
